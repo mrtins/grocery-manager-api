f1b2c468002f53e63ed3296228bdb5ac431f011e
Vitor Martins <vitor.martins@stef.com>
2022-01-13 19:26:22 +0000

    Initial commit

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,47 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({ data: createCategoryDto });
+  }
+
+  findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.category.findUnique({ where: { id } });
+  }
+
+  findByUser(userId: number) {
+    return this.prisma.category.findMany({ where: { userId } });
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
+  }
+
+  async remove(id: number) {
+    const data = await this.prisma.category.findUnique({ where: { id } });
+
+    if (!data) {
+      throw new NotFoundException();
+    }
+
+    return this.prisma.category.delete({ where: { id } });
+  }
+}
