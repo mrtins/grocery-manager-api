083d56a9147d2aa4902646cb519bee202664a9a8
Vitor Martins <vitorfigmartins@gmail.com>
2022-02-03 22:39:58 +0000

    Adding paths to tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from 'modules/categories/categories.module';
+import { ProductOrderModule } from 'modules/product-order/product-order.module';
+import { ProductModule } from 'modules/product/product.module';
+import { PurchaseModule } from 'modules/purchase/purchase.module';
+import { SizeMeasureModule } from 'modules/size-measure/size-measure.module';
+import { StoreModule } from 'modules/store/store.module';
+import { UserModule } from 'modules/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { PrismaService } from './prisma/prisma.service';
-import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CategoriesModule, UserModule],
+  imports: [
+    CategoriesModule,
+    UserModule,
+    StoreModule,
+    SizeMeasureModule,
+    PurchaseModule,
+    ProductOrderModule,
+    ProductModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
